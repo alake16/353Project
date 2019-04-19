79867181bbc044426b4c2a350cb7aca1bc785c9d
Andrew Lake <aclake@umich.edu>
2019-04-19 14:30:35 -0500

    Fixed account page

--- PROJ353/flaskDemo/forms.py
+++ PROJ353/flaskDemo/forms.py
@@ -84,11 +84,10 @@ class LoginForm(FlaskForm):
 
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username',
+    name = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    address = StringField('Address',
+                        validators=[DataRequired(), Length(min=2, max=40)])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
--- PROJ353/flaskDemo/routes.py
+++ PROJ353/flaskDemo/routes.py
@@ -207,20 +207,15 @@ def save_picture(form_picture):
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
+        current_user.name = form.name.data
+        current_user.address = form.address.data
         db.session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
     elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account',
-                           image_file=image_file, form=form)
+        form.name.data = current_user.name
+        form.address.data = current_user.address
+    return render_template('account.html', title='Account', form=form)
 
 
 @app.route("/dept/new", methods=['GET', 'POST'])
